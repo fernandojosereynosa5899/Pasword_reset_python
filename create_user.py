bfa3ceaa3c13c8d9beb91cc73eac426ac1b16336
fernandojosereynosa5899 <fernando.jose.reynosa@gmail.com>
2026-09-24 21:54:13 -0600

    Codigo para poner un usuario de prueba en la db

---
+++ create_user.py
@@ -0,0 +1,27 @@
+import models
+import security
+from main import SessionLocal
+
+def create_test_user():
+    db =SessionLocal()
+
+    # Verificamos que el usuario no exista
+    existing_user = db.query(models.User).filter(models.User.email == "fernandoprueba@gmail.com").first()
+    if existing_user:
+        print("El usuario de prueba ya existe")
+        db.close()
+        return
+
+    # Creamos el usuario con la contraseña encriptada
+    test_user = models.User(
+        email="fernandoprueba@gmail.com",
+        hashed_password=security.get_password_hash("Mi_contrasena_segura")
+    )
+
+    db.add(test_user)
+    db.commit()
+    db.close()
+    print(f"Usuario {test_user.email} Creado con exito")
+
+if __name__ == "__main__":
+    create_test_user()
